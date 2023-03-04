707c4c6f6d60626d66c8ff4d05b8dcea63108671
Jhonatas <jhonatasoliveira021@gmail.com>
2023-03-04 12:06:06 -0300

    chore: add back-end

---
+++ back-end/src/teste.spec.ts
@@ -0,0 +1,8 @@
+import { equal } from "assert";
+
+describe("Teste primário", () => {
+  it("Sould be albe to test", () => {
+    const somaResult = 2 + 2;
+    expect(somaResult).toEqual(4);
+  });
+});
